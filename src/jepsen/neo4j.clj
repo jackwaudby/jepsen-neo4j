532556d43b4ef78664d788f184ef3bba684cfdc5
jackwaudby <j.waudby2@ncl.ac.uk>
2021-11-10 19:14:38 +0000

    fix #3: install, start, teardown db

--- src/jepsen/neo4j.clj
+++ src/jepsen/neo4j.clj
@@ -1,16 +1,49 @@
 (ns jepsen.neo4j
- (:require [neo4clj.client :as client]
-            [jepsen.cli :as cli]
-            [jepsen.tests :as tests]))
+  (:require [clojure.tools.logging :refer :all]
+            [clojure.string :as str]
+            [neo4clj.client :as client]
+            [jepsen [cli :as cli]
+             [control :as c]
+             [db :as db]
+             [tests :as tests]]
+            [jepsen.control.util :as cu]
+            [jepsen.os.debian :as debian]))
 
-(def conn (client/connect "bolt://localhost:7687" "neo4j" "admin"))
+(def dir "/opt/neo4j")
+(def logfile (str dir "/logs/neo4j.log"))
+
+(defn db
+  "Neo4j DB for a particular version."
+  [version]
+  (reify db/DB
+    (setup! [_ test node]
+      (info node "installing neo4j" version)
+      (c/su
+       (let [url (str "http://dist.neo4j.org/neo4j-community-" version "-unix.tar.gz")]
+         (cu/install-archive! url dir)))
+      (info (c/su (c/exec* (str dir "/bin/neo4j start"))))
+      (Thread/sleep 10000))
+
+    (teardown! [_ test node]
+      (info (c/su (c/exec* (str dir "/bin/neo4j stop || true"))))
+      (info node "tearing down neo4j")
+      (c/su (c/exec :rm :-rf dir)))
+
+    db/LogFiles
+    (log-files [_ test node]
+      [logfile])))
+
+
 
 (defn neo4j-test
   "Given an options map from the command line runner (e.g. :nodes, :ssh,
   :concurrency, ...), constructs a test map."
   [opts]
   (merge tests/noop-test
-         {:pure-generators true}
+         {:pure-generators true
+          :name "neo4j"
+          :os debian/os
+          :db (db "4.3.7")}
          opts))
 
 (defn -main
