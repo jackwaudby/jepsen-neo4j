d2a5ec3327555c6507b4ae9a4340438751220780
jackwaudby <j.waudby2@ncl.ac.uk>
2021-11-10 10:41:15 +0000

    initialise project

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject jepsen.neo4j "0.1.0-SNAPSHOT"
+  :description "A Jepsen test for Neo4j."
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [jepsen "0.2.1-SNAPSHOT"]]
+  :main jepsen.neo4j
+  :repl-options {:init-ns jepsen.neo4j}
+)
---
+++ test/jepsen/neo4j_test.clj
@@ -0,0 +1,7 @@
+(ns jepsen.neo4j-test
+  (:require [clojure.test :refer :all]
+            [jepsen.neo4j :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
