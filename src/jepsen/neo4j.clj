a11ced9e4028cc2a79cedcf51d8e496879d40670
jackwaudby <j.waudby2@ncl.ac.uk>
2021-11-10 16:14:49 +0000

    fix #2: added test scaffolding

--- src/jepsen/neo4j.clj
+++ src/jepsen/neo4j.clj
@@ -1,7 +1,22 @@
 (ns jepsen.neo4j
-  (:gen-class))
+ (:require [neo4clj.client :as client]
+            [jepsen.cli :as cli]
+            [jepsen.tests :as tests]))
+
+(def conn (client/connect "bolt://localhost:7687" "neo4j" "admin"))
+
+(defn neo4j-test
+  "Given an options map from the command line runner (e.g. :nodes, :ssh,
+  :concurrency, ...), constructs a test map."
+  [opts]
+  (merge tests/noop-test
+         {:pure-generators true}
+         opts))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Handles command line arguments. Can either run a test, or a web server for
+  browsing results."
   [& args]
-  (println "Hello, World!"))
+  (cli/run! (merge (cli/single-test-cmd {:test-fn neo4j-test})
+                   (cli/serve-cmd))
+            args))
